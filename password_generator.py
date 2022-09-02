76db03859f7ccdf6467affa177d196692d243eb1
erma0x <35337869+erma0x@users.noreply.github.com>
2022-09-02 17:41:15 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,20 +1,21 @@
 # Password genrator
 
-
+import random as rnd
+import sys
+           
 if __name__ == '__main__':
-           import random as rnd
-           import sys
-
-           length = int(sys.argv[1])
-
-           lower_letters = 'qwertyuiopasdfghjklzxcvbnm'
-           upper_letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-           numbers = '1234567890'
-           symbols = "[]{}();,./@#$%^&*"
-
-
-           all_elements = lower_letters + upper_letters + numbers + symbols
-
-
-           password = "".join(rnd.sample(all_elements,length))
-           print(password)
+    length = 20
+    
+    if len(sys.argv)>1:
+    	length = int(sys.argv[1])
+    	
+    lower_letters = 'qwertyuiopasdfghjklzxcvbnm'
+    upper_letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    numbers = '1234567890'
+    
+    all_elements = lower_letters + upper_letters + numbers
+    
+    password = "".join(rnd.sample(all_elements,length))
+    
+    print('\n\t password generated!')
+    print('\n\t',password,'\n\n')
